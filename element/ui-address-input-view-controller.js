b7773ae158c8a1e835a96ab2df0fc778a4c095c4
Jose Barrios <jose@cognilab.com>
2017-08-22 07:48:10 -0700

    Added placeholder attribute

--- element/ui-address-input-view-controller.js
+++ element/ui-address-input-view-controller.js
@@ -3,36 +3,30 @@ const uiAddressInputTemplate = uiAddressInputDocument.ownerDocument.querySelecto
 
 class UIAddressInput extends HTMLElement{
 
-	static get observedAttributes(){ return ['value']; }
+	static get observedAttributes(){ return ['value', 'placeholder']; }
 
   constructor(model){
     super();
 		this.model = new PostalAddress();
 		const view = document.importNode(uiAddressInputTemplate.content, true);
-
-		//SHADOW ROOT
+		//LIGHT DOM
+		this.appendChild(view);
+		//SHADOW DOM
 		//this.shadowRoot = this.attachShadow({mode: 'open'});
 		//this.shadowRoot.appendChild(view);
-
-		this.appendChild(view);
-
 		this.connected = false;
 	}
-
 
  connectedCallback() {
 	 this.connected = true;
 	 this.$input = this.querySelector('input');
-
 	 //SHADOW ROOT
 	 //this.$input = this.shadowRoot.querySelector('input');
-
 	 this.autocomplete = new google.maps.places.Autocomplete( (this.$input), {types: ['geocode']});
 	 // When the user selects an address from the dropdown, populate the address fields in the form.
 	 this.autocomplete.addListener('place_changed', e=> {this.setAddress()});
-	 //this.$input.addEventListener('focus', e => {this.geolocate(e)});
+	 this.$input.addEventListener('focus', e => {this.geolocate(e)});
   }
-
 
 	// Bias the autocomplete object to the user's geographical location,
 	// as supplied by the browser's 'navigator.geolocation' object.
@@ -52,8 +46,8 @@ class UIAddressInput extends HTMLElement{
 		}
 	}
 
+	// Get the place details from the autocomplete object.
 	setAddress(e){
-		// Get the place details from the autocomplete object.
 		var place = this.autocomplete.getPlace();
 		let addressModel = {};
 		place.address_components.forEach(item => {
@@ -73,21 +67,25 @@ class UIAddressInput extends HTMLElement{
   attributeChangedCallback(attrName, oldVal, newVal) {
 		switch(attrName){
 			case 'value':
-				//Wait until the fucking Google API is loaded
-				let waitForAPIToLoad = () => {
-					checkAPI();
-				}
-				let updateModel = () => {
-					this.value = JSON.parse(newVal);
-					this._updateEvent();
-				}
 				let checkAPI = () => {
 					//Check if API is loaded
 					if(google){ updateModel(); }
 					//Try again next chance you get
 					else{ requestAnimationFrame(waitForAPIToLoad); }
 				}
+				//Wait until the fucking Google API is loaded
+				let waitForAPIToLoad = () => {
+					checkAPI();
+				}
+				//If API is loaded, update
+				let updateModel = () => {
+					this.value = JSON.parse(newVal);
+					this._updateEvent();
+				}
 				checkAPI();
+				break;
+			case 'placeholder':
+				this.placeholder = newVal;
 				break;
 
 			default:
@@ -99,9 +97,8 @@ class UIAddressInput extends HTMLElement{
 		this.dispatchEvent(new CustomEvent('update', {detail: this.value, bubbles:false }));
 	}
 
-
-	get shadowRoot(){return this._shadowRoot;}
-	set shadowRoot(value){ this._shadowRoot = value}
+	//get shadowRoot(){return this._shadowRoot;}
+	//set shadowRoot(value){ this._shadowRoot = value}
 
 	get value(){return PostalAddress.assignedProperties(this.model);}
 	set value(value){
@@ -117,9 +114,28 @@ class UIAddressInput extends HTMLElement{
 		}
 	}
 
-  disconnectedCallback() {
-    console.log('disconnected');
-  }
+	get placeholder(){ return this._placeholder; }
+	set placeholder(value){
+		let checkElementUpgrade = () => {
+			//Check if element is connected
+			if(this.connected){ updatePlaceholder(); }
+			//Try again next chance you get
+			else{ requestAnimationFrame(waitForElementUpgrade); }
+		}
+		//Wait until the fucking Google API is loaded
+		let waitForElementUpgrade = () => {
+			checkElementUpgrade();
+		}
+		//If API is loaded, update
+		let updatePlaceholder = () => {
+			this.$input.placeholder = value;
+		}
+		checkElementUpgrade();
+	}
+
+	disconnectedCallback() {
+		console.log('disconnected');
+	}
 }
 
 window.customElements.define('ui-address-input', UIAddressInput);
